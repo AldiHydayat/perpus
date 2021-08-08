5fe94d1a43bab9ba4e51a6d24aa230b2cea39412
JohnEpong <aldyh128@gmail.com>
2021-08-08 16:49:44 +0700

    ready to deploy

--- src/Context/context.jsx
+++ src/Context/context.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react';
 export const perpusContext = createContext();
 
 export const Perpusprovider = ({ children }) => {
-  const url = 'http://localhost:3100/api';
+  const url = 'https://backend-perpus.herokuapp.com/api';
 
   return <perpusContext.Provider value={{ url }}>{children}</perpusContext.Provider>;
 };
